1c7760eaa1e66d91dada78451d7779c6a93d9bb4
alexandrenguyen68817 <alexandrenguyen68817@gmail.com>
2016-05-11 05:10:29 -0400

    Ajout de la requete de films sous la forme d'un tableau

---
+++ films.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="latin1">
+  <meta name="viewport" content="width=device-width">
+  <title>Films</title>
+</head>
+<body>
+  <h1>Films</h1>
+  <table>
+  <thead>
+    <tr>
+      <th>Titre</th>
+      <th>Annee</th>
+      <th>Genre</th>
+      <th>Realisateur</th>
+    </tr>
+  </thead>
+    <?php
+$link=mysqli_connect("dwarves.iut-fbleau.fr","nguyen","080382Alex","nguyen");
+  if(!$link){
+    die("<p>connexion impossible</p>");
+  }
+  $resultat=mysqli_query($link,"SELECT titre,annee,genre,nom FROM Film f JOIN Artiste a ON f.idMes = a.idArtiste");
+  if($resultat){
+    while($film=mysqli_fetch_assoc($resultat)){
+      echo "<tr>";
+	    echo "<td>".$film['titre']."</td>";
+	    echo "<td>".$film['annee']."</td>";
+    	echo "<td>".$film['genre']."</td>";
+	    echo "<td>".$film['nom']."</td>";
+	    echo "</tr>";
+    }
+  }
+   else{
+        die("<p>Erreur dans l'éxécution de la requête. </p>");
+   }
+    mysqli_close($link);
+?>
+  </table>
+</body>
+
+</html>
